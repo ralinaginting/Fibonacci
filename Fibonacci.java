2e76a7af7b6c1ade427c94cdfd95cac4cca69cf5
Ralina Ginting <oh.lina158@gmail.com>
2017-04-08 11:55:46 +0700

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,26 +1,10 @@
-
-package fibonacci;
-
 import java.util.Scanner;
-
-public class Fibonacci {
-
-    public static void main(String[] args) {
-        Scanner show = new Scanner(System.in);
-        System.out.print("Masukan Jumlah Deret Fibonacci:");
-        int n = show.nextInt();
-        long fib[] = new long[n];
-         
-        fib[0] = 0;
-        fib[1] = 1;
-         
-        for(int i = 2; i < n; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
-        }
-         
-        for (int i = 0; i < n; i++) {
-            System.out.print(fib[i] +  " ");
-        }
-    }
- }
-    
+    public class InputOut {
+         public static void main(String args[]) {
+             Scanner masukan = new Scanner (System.in);
+                String x;
+                  System.out.print("Masukan Sebuah Kata : ");
+                    x = masukan.next();
+                  System.out.println("Kata Yang Anda Masukan Adalah : "+x);
+         }       
+    } 
